3709a604c1bf4405d5c6d5e9028472dbd3580cf1
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-06-19 23:01:06 +0100

    Update toxicants/src/arguments.py

--- toxicants/src/arguments.py
+++ toxicants/src/arguments.py
@@ -35,5 +35,13 @@ class Arguments:
 
         text = yaml.safe_load(elements.text)
 
+        # Are the URL strings valid?
+        data_url = text['dataURL']
+        assert requests.head(url=data_url).status_code == 200, "The YAML dataURL string  is invalid"
+
+        schema_url = text['schemaURL']
+        assert requests.head(url=schema_url).status_code == 200, "The YAML schemaURL string  is invalid"
+
+        # Hence, add the URL strings to the data parameters collection
         DataParameters = collections.namedtuple(typename='DataParameters', field_names=['data_url', 'schema_url'])
         return DataParameters._make((text['dataURL'], text['schemaURL']))
