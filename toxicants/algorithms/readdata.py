e8ef6b54fdd04150328ed4ce0acf89a141d8368a
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-06-20 14:32:05 +0100

    Add toxicants/algorithms/readdata.py

---
+++ toxicants/algorithms/readdata.py
@@ -0,0 +1,63 @@
+"""Module readdata"""
+import collections
+
+import pandas as pd
+
+
+class ReadData:
+    """
+    Class ReadData
+    """
+
+    def __init__(self, specifications: collections.namedtuple):
+        """
+        Constructor
+
+        :param specifications:
+                    specifications.data_url -> The data URL |
+                    specifications.usecols -> The data file fields to be read |
+                    specifications.dtype -> A dictionary of the fields to be read and their data types |
+                    specifications.rename -> Do one or more fields have to be renamed?  |
+                    specifications.dictionary_of_names -> If 'rename' is True, a dictionary of the fields to be renamed and
+                    their new names
+        """
+
+        self.specifications = specifications
+
+    def __read(self) -> pd.DataFrame:
+        """
+        Reads the data
+        :return:
+        """
+
+        try:
+            data = pd.read_csv(filepath_or_buffer=self.specifications.data_url, header=0, usecols=self.specifications.usecols,
+                               dtype=self.specifications.dtype, encoding='UTF-8')
+        except OSError as err:
+            raise Exception(err.strerror) in err
+
+        return data
+
+    def __rename(self, blob: pd.DataFrame) -> pd.DataFrame:
+        """
+        Renames the fields of DataFrame blob
+        :param blob:
+        :return:
+        """
+
+        return blob.rename(columns=self.specifications.dictionar_of_names)
+
+    def exc(self):
+        """
+        Entry point.
+        :return:
+        """
+
+        # Read the data into a DataFrame
+        data = self.__read()
+
+        # If required, rename fields
+        if self.specifications.rename:
+            data = self.__rename(blob=data.copy())
+
+        return data
