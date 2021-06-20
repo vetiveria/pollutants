d8630eadb033306a178e05fe62e25dc1cdfa4871
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-06-20 19:39:25 +0100

    Update toxicants/io/arguments.py

--- toxicants/io/arguments.py
+++ toxicants/io/arguments.py
@@ -57,8 +57,10 @@ class Arguments:
             assert isinstance(dictionary_of_names, dict), "A dictionary names, wherein the old name is the key and the new " \
                                                           "name is the value, is required for renaming data fields"
             assert len(dictionary_of_names) > 0, "The dictionary of names is empty"
-
+        else:
+            dictionary_of_names = {}
 
         # Hence, add the URL strings to the data parameters collection
-        DataParameters = collections.namedtuple(typename='DataParameters', field_names=['data_url', 'schema_url', 'rename', 'dictionary_of_names'])
+        DataParameters = collections.namedtuple(typename='DataParameters',
+                                                field_names=['data_url', 'schema_url', 'rename', 'dictionary_of_names'])
         return DataParameters._make((data_url, schema_url, rename, dictionary_of_names))
